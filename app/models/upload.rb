33f968f7c05686810320798180c63f54265f93b6
Miika Pihlaja <zonpantli@gmail.com>
2011-06-24 01:40:24 +0300

    fixed spelling error in upload.rb

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,23 +1,24 @@
 class Upload < ActiveRecord::Base
  require 'RMagick'
  
-attr_accessor :x1, :y1, :width, :height
+  attr_accessor :x1, :y1, :width, :height
 
-def update_attributes(att)
+  def update_attributes(att)
  
-  scaled_img = Magick::ImageList.new(self.photo.path)
-  orig_img = Magick::ImageList.new(self.photo.path(:original))
-  scale = orig_img.columns.to_f / scaled_img.columns
+    scaled_img = Magick::ImageList.new(self.photo.path)
+    orig_img = Magick::ImageList.new(self.photo.path(:original))
+    scale = orig_img.columns.to_f / scaled_img.columns
  
-  args = [ att[:x1], att[:y1], att[:width], att[:height] ]
-  args = args.collect { |a| a.to_i * scale }
+    args = [ att[:x1], att[:y1], att[:width], att[:height] ]
+    args = args.collect { |a| a.to_i * scale }
  
-  orig_img.crop!(*args)
-  orig_img.write(self.photo.path(:original))
+    orig_img.crop!(*args)
+    orig_img.write(self.photo.path(:original))
  
-  self.photo.reprocess!
-  self.save
+    self.photo.reprocess!
+    self.save
  
-  super(att)
+    super(att)
+  end
+
 end
-
